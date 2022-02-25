8a9adb3b33e83b10c9f97dc0b32b2e91a780bf15
咸鱼先生 <3132615912@qq.com>
2022-02-25 11:09:43 +0800

    1.在activity_main.xml中是对LinearLayout布局的示例实践 2.创建了ActivityCollector管理活动类，BaseActivity集成基础共性

---
+++ app/src/main/java/com/example/firstactivity/BaseActivity.java
@@ -0,0 +1,25 @@
+package com.example.firstactivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+        Log.d("正在运行",getClass().getSimpleName());
+    }
+
+    /** 或android.os.Process.killProcess(android.os.Process.myPid());
+     * 杀死自己的进程
+     * */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+}
